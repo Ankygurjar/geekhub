d743c25eab5bf2a6002e0d203ddbf3a3a850a534
Ankygurjar <ankitchoudhary699.ac@gmail.com>
2022-02-19 18:53:49 -0500

    Added Client Pages

--- geekhub/src/App.js
+++ geekhub/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import Home from './Components/Home/Home.geekhub.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h2>This is Home.</h2>
+      <Home />
     </div>
   );
 }
